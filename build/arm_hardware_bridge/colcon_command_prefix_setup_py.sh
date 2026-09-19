ee3244e885a0668833dff34f58edc85af9372461
migara10 <migaraten@gmail.com>
2026-09-19 21:00:58 +0300

    remove build, insall and log folders

--- build/arm_hardware_bridge/colcon_command_prefix_setup_py.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/migara/robot_ws/install/arm_description/share/arm_description/package.sh"
-. "/home/migara/robot_ws/install/arm_moveit_config/share/arm_moveit_config/package.sh"
--- install/arm_moveit_config/share/arm_moveit_config/launch/setup_assistant.launch.py
+++
@@ -1,1 +0,0 @@
-/home/migara/robot_ws/src/arm_moveit_config/launch/setup_assistant.launch.py
